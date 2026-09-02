508175319b0add3ec88767738cd68a5cd242b2ce
chibuikeeugene <chibuikeeugene@gmail.com>
2026-09-02 15:30:34 +0200

    env isolation and aliases - updated

--- 06-remote-state/modules/network/versions.tf
+++ 06-remote-state/modules/network/versions.tf
@@ -3,7 +3,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">=6.0"
-      configuration_aliases = [ aws.secondary ] # tells child module to expects additional provider config
+      # configuration_aliases = [ aws.secondary ] # tells child module to expects additional provider config
     }
   }
 }
