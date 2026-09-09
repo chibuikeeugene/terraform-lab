f0aabbd8c6472222320848995c6269deeedfa827
chibuikeeugene <chibuikeeugene@gmail.com>
2026-09-09 15:29:56 +0200

    importing existing resources into our terraform infrastructure - first commit

--- 08-state-operations/imports.tf
+++ 08-state-operations/imports.tf
@@ -1,17 +1,10 @@
 # import an already existing service - s3 bucket
 import {
-  to = aws_s3_bucket.application_data
+  to = module.storage.aws_s3_bucket.this
   id = var.import_bucket_name
 }
 
-# rename/move resource to aws_s3_bucket.application_data
-moved {
-  from = aws_s3_bucket.legacy
-  to = aws_s3_bucket.application_data
-}
-
-# move aws_s3_bucket resource to module
-moved {
-  from = aws_s3_bucket.application_data
-  to = module.storage.aws_s3_bucket.this
+import {
+  to = module.storage.aws_vpc.test_vpc
+  id = var.vpc_id
 }
